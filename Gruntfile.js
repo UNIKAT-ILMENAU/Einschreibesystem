83a9d740f7a3ed54d6216e4750ce5bd70a4915bd
Dirk Heinke <dirkheinke@gmail.com>
2016-10-10 16:43:03 +0200

    Add uglify to watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,7 +34,7 @@ module.exports = function (grunt) {
         watch: {
             dev: {
                 files: ['Gruntfile.js', 'web/components/controllers/*.js'],
-                tasks: ['concat:controllers', 'concat:services']
+                tasks: ['concat:controllers', 'concat:services', 'uglify']
             }
         },
         concat: {
